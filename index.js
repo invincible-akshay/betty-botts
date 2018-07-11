b60f2d1a5e7d820b33bca49976b34a698be5ed22
invincible-akshay <invincible-akshay@users.noreply.github.com>
2018-07-11 22:11:29 +0530

    Webhook successful

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+
+const verificationController = require('./controllers/verification');
+app.get('/', verificationController);
